d9c0b1c5840bf953f14fbd5f83364da8e7b80030
Richard Kojedzinszky <richard.kojedzinszky@euronetrt.hu>
2022-07-06 11:55:26 +0200

    PoolDatasetService: process extra options as in branch master

--- src/middlewared/middlewared/plugins/pool.py
+++ src/middlewared/middlewared/plugins/pool.py
@@ -17,6 +17,7 @@ import shutil
 import subprocess
 import tempfile
 import uuid
+import copy
 
 from collections import defaultdict
 
@@ -2817,6 +2818,10 @@ class PoolDatasetService(CRUDService):
         # Optimization for cases in which they can be filtered at zfs.dataset.query
         zfsfilters = []
         filters = filters or []
+        extra = copy.deepcopy(options.get('extra', {}))
+        retrieve_children = extra.get('retrieve_children', True)
+        props = extra.get('properties')
+        snapshots = extra.get('snapshots')
         sys_config = self.middleware.call_sync('systemdataset.config')
         if sys_config['basename']:
             filters.extend([
@@ -2834,8 +2839,15 @@ class PoolDatasetService(CRUDService):
 
         return filter_list(
             self.__transform(self.middleware.call_sync(
-                'zfs.dataset.query', zfsfilters, {'extra': {'flat': options.get('extra', {}).get('flat', True)}})
-            ), filters, options
+                'zfs.dataset.query', zfsfilters, {
+                    'extra': {
+                        'flat': extra.get('flat', True),
+                        'retrieve_children': retrieve_children,
+                        'properties': props,
+                        'snapshots': snapshots,
+                    }
+                }
+            )), filters, options
         )
 
     def __transform(self, datasets):
